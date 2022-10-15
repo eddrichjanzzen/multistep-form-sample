670e1065cef0364267ff5ab1d457008e642ce2f1
Eddrich Janzzen Ang <eddrichang@gmail.com>
2022-10-15 17:00:21 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import useMultiStepForm from './useMultiStepForm';
+
+function App() {
+  const sample = [<div>One</div>, <div>Two</div>];
+
+  const { steps, currentStepIndex, step, isFirstStep, back, next } =
+    useMultiStepForm(sample);
+
+  return (
+    <div
+      style={{
+        position: 'relative',
+        background: 'white',
+        border: '1px solid black',
+        padding: '2rem',
+        margin: '1rem',
+        borderRadius: '.5rem',
+        fontFamily: 'Arial',
+      }}
+    >
+      <form>
+        <div
+          style={{
+            position: 'absolute',
+            top: '0.5rem',
+            right: '0.5rem',
+          }}
+        >
+          {currentStepIndex + 1} / {steps.length}
+        </div>
+        {step}
+        <div
+          style={{
+            marginTop: '1rem',
+            display: 'flex',
+            gap: '0.5rem',
+            justifyContent: 'center',
+          }}
+        >
+          {!isFirstStep && (
+            <button type="button" onClick={back}>
+              Back
+            </button>
+          )}
+          <button type="button" onClick={next}>
+            Next
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+export default App;
