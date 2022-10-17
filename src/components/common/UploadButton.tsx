d8e760a917cd06dec8a17e8a2958dea739ba1a63
Eddrich Janzzen Ang <eddrichang@gmail.com>
2022-10-17 16:47:45 +0800

    Update upload button

--- src/components/common/UploadButton.tsx
+++ src/components/common/UploadButton.tsx
@@ -1,25 +1,29 @@
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import UploadIcon from '@mui/icons-material/Upload';
 import Stack from '@mui/material/Stack';
-import { FormControl, FormLabel } from '@mui/material';
+import { Box, FormControl, FormLabel, Typography } from '@mui/material';
 
 type UploadButtonProps = {
   label: string;
+  onChange?: React.ChangeEventHandler<HTMLInputElement> | undefined;
 };
 
-const UploadButton = ({ label }: UploadButtonProps) => {
+const UploadButton = ({ label, onChange }: UploadButtonProps) => {
   return (
     <Stack direction="row" alignItems="center" spacing={2}>
       <FormControl>
-        <FormLabel>{label}</FormLabel>
+        <Box pl={1}>
+          <FormLabel>{label}</FormLabel>
+        </Box>
         <IconButton
           color="primary"
           aria-label="upload picture"
           component="label"
+          disableRipple
         >
-          <input hidden accept="image/*" type="file" />
-          <PhotoCamera />
+          <UploadIcon />
+          <Typography variant="caption">UPLOAD A FILE</Typography>
+          <input hidden accept="image/*" type="file" onChange={onChange} />
         </IconButton>
       </FormControl>
     </Stack>
