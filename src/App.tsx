e8f406783fd4b594f87b4b6f8f1fc7b917275b1c
Eddrich Janzzen Ang <eddrichang@gmail.com>
2022-11-01 20:52:08 +0800

    Add react query dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Home from './pages/Home';
-
+import './App.css';
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Home />
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   );
 }
